3833a2fbb7ca7131723a7d6c188dcbc541ec8af3
juanpchcia <juanpchica@hotmail.com>
2019-02-17 17:22:26 -0500

    Auto-generated commit

---
+++ assets/template.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    // Toggle the side navigation
+    $("#sidebarToggle").on('click', function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+      });
+});
